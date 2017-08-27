c654a4569a1b8c9a632a0c40ebcadff57bf91127
hamzabhaimia <hamzabhaimia@gmail.com>
2017-08-28 00:23:49 +0530

    'first'

---
+++ templates_project/first_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    context_dict={'text':'hello world','number':100}
+    return render(request,'first_app/index.html',context_dict)
+
+def other(request):
+    return render(request,'first_app/other.html')
+
+def relative(request):
+    return render(request,'first_app/relative_url_templates.html')
